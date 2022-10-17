93de6de4c8db83dd4b8bc58b0a5e6bcfa6dca195
manishpatel0 <mp0000091@gmail.com>
2022-10-17 15:06:31 +0530

    Multidimensional dynamic arrays
    tow dimensional dynamic array

--- C_and_CPP/Pointer/prog_src01.cpp
+++ C_and_CPP/Pointer/prog_src01.cpp
@@ -4,11 +4,28 @@ using namespace std;
 
 int main(){
 
-	int myArray[5];
+	// int myArray[5];
 	int size;
 	cout << "Size: ";
 	cin >> size;
-	int myArray[size];
+	// my create array dynamicl array 
+	int *myArray= new int[size];
+
+// 
+	for (int i = 0; i < size; i++)
+	{
+		 cout << "Array[" << i << "] " ;
+		 cin >> myArray[i];
+	}
+
+	for (int i = 0; i < size; i++)
+	{
+		// cout << myArray[i] << " " ;
+		cout << *(myArray + i) << " " ;
+	}
+// delete array
+	delete[]myArray;
+	myArray = NULL;
 
 
 	return 0;
