32572c578bebc86cbc345c97dd208790d1c56a23
manishpatel0 <mp0000091@gmail.com>
2022-10-18 15:13:17 +0530

    Update User:

--- C_and_CPP/Pointer/prog_src02.cpp
+++ C_and_CPP/Pointer/prog_src02.cpp
@@ -1,4 +1,22 @@
+// dynamic arrays
 #include <iostream>
 using namespace std;
 
 
+int main(){
+
+	int rows , cols;
+	cout <"rows, cols: ";
+	cin >> rows >> cols;
+
+
+	int **table=new int*[rows][cols];
+	for(int i = 0; i < rows; i++){
+		table[i] = new int[cols];
+	}
+
+	// table[1][2] = 88;
+
+
+	return 0;
+}
