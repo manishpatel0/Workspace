30c7e44a5bdbb3e856d730bae75d596c3c8d7520
manishpatel0 <mp0000091@gmail.com>
2022-10-25 18:18:47 +0530

    #

--- C_and_CPP/usr/prog_00.c
+++ C_and_CPP/usr/prog_00.c
@@ -7,3 +7,17 @@ struct Node {
 
 }
 
+void create_node(int x, int y, struct Node **temp){
+	struct Node *r,*z;
+	z = *temp;
+	if(z==NUll){
+		r=(struct Node*)malloc(sizeof(strcut Node));
+		r->coeff =x;
+		r->pow = y;
+		*temp = r;
+		r->next = (struct Node*)malloc(sizeof(struct Node));
+		r= r->next;
+		r->next = NULL;
+	}
+	
+}
