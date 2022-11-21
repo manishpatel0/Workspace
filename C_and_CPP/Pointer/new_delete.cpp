2a875d18fb8919c368f03362f39ba67543d539fd
manishpatel0 <mp0000091@gmail.com>
2022-11-21 23:53:39 +0530

    deallocate memoty

--- C_and_CPP/Pointer/new_delete.cpp
+++ C_and_CPP/Pointer/new_delete.cpp
@@ -2,11 +2,12 @@
 using namespace std;
 
 int main(){
-	int a = 10;
-	int *p = new int();
+	int a = 10;	//stored in stack
+
+	int *p = new int();	//allocate memory in heap
 	*p = 10;
 
-	delete(p);
+	delete(p);	//deallocate memory
 	p = new int[4];
 	delete[]p;
 	p = NULL;
